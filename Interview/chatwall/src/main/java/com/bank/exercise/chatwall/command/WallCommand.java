b2300fa576bac11e7b81dff0ff6121e28071726e
ZoliEngineer <juzoli@gmail.com>
2013-10-25 23:49:02 +0200

    created base of wall command

--- Interview/chatwall/src/main/java/com/bank/exercise/chatwall/command/WallCommand.java
+++ Interview/chatwall/src/main/java/com/bank/exercise/chatwall/command/WallCommand.java
@@ -1,22 +1,36 @@
 package com.bank.exercise.chatwall.command;
 
+import com.bank.exercise.chatwall.model.User;
 import com.bank.exercise.chatwall.storage.Storage;
 import com.google.common.base.Strings;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 
 class WallCommand implements Command {
     private static final String WALL_COMMAND_PART = "wall";
 
+    private final String userName;
+
+    public WallCommand(String[] splitedCommandLine) {
+        this.userName = splitedCommandLine[0];
+    }
+
     @Override
     public boolean isExit() {
-        // TODO Auto-generated method stub
         return false;
     }
 
     @Override
     public List<String> execute(Storage storage) {
+        User user = storage.getUser(userName);
+        Set<User> subscriptions = user.getSubscriptions();
+       // Set<ChatLine>
+       // for(User user : subscriptions){
+       //
+       // }
+
 
         return Collections.emptyList();
 
@@ -28,6 +42,10 @@ class WallCommand implements Command {
         } else {
             return commandLine.contains(WALL_COMMAND_PART); // Out of scope: deeper check in case of invalid command
         }
+    }
+
+    public static Command create(String commandLine) {
+        return new WallCommand(commandLine.split(WALL_COMMAND_PART)); // Out of scope: input validation
     }
 
 }
